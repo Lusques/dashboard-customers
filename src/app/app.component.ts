f6f418973ed719eeb0df0e4d07a793021cffeca1
Lucas Silva <lucascarlosjobs@gmail.com>
2025-05-13 14:14:47 -0300

    feat(layout): creates banner on auth layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth/auth-layout/auth-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AuthLayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dashboard-customers';
